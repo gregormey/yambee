09d97b5dd27b17fe629e4ad8753cbf4128a0774e
Gregor Meyenberg <gregor@meyenberg.de>
2014-07-02 20:53:24 +0200

    bug fix forbidden

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ app.param('guest_id', function(req, res, next, guestId) {
 });
 
 
+//deny acces if it is no valid guest
+app.use(function(req, res, next){
+  if(!req.session.guest && !req.params.guest_id){
+    res.send(403);
+  }else{
+    next();
+  }
+});
+
+
 
 /**
  * route configuration
@@ -63,14 +73,6 @@ app.get('/agree', invites.agree);
 app.get('/refuse', invites.refuse);
 app.get('/info', invites.info);
 
-//deny acces if it is no valid guest
-app.use(function(req, res, next){
-	if(!req.session.guest && !req.params.guest_id){
-		res.send(403);
-	}else{
-		next();
-	}
-});
 
 
 /**
